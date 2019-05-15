b3b745fda248f01351f7eed521eaa2b9dbf9908f
dexter-mayo <dexter.m20@k12.xrds.org>
2019-05-15 10:48:27 -0700

    Initial Commit

---
+++ DexterFinalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DexterFinalApp
+//
+//  Created by Dexter Mayo on 5/15/19.
+//  Copyright © 2019 Dexter Mayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
